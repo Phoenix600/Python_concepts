90251bb0166f303e50b20f4256c66e11800c328c
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-05-21 04:25:43 +0530

    Add files via upload

---
+++ 24_more_about_modules.py~
@@ -0,0 +1,8 @@
+import constant as C
+''' When our programe grows bigger, its a good idea to break it into different modules 
+    A module is a file containing Python definations and statements 
+    Python modules have a filename and ebd with extension .py
+'''
+
+print(C.PI)
+
---
+++ 25_special_operators_in_python.py
@@ -0,0 +1,34 @@
+''' Python provides special two type of operators  
+    1. Identity Operator    is , is not
+    2. Membership OPerator 
+
+'''
+
+# Identity Operator in Python 
+
+old_pass = "Nahi_Mahit"
+new_pass = "Nahi"+ "_" + "Mahit"
+
+
+my_org_pass = "This is not a password, it is my privacy"
+print(old_pass is new_pass)
+
+print(old_pass is not my_org_pass)
+
+
+# Membership Operators 
+'''     'in' and 'not in' are the membership operator in python . They are used to test or check whether 
+        the elements or variable or value is present in a sequence or not 
+
+        In python we reffer string,list,tuple,set and dictionary as seqence 
+'''
+
+message = "How are you?"
+
+bool_result = '?' in message 
+
+print(bool_result)
+
+sample_list = ['pranay','single','150+',20]
+bool_reult = 'single' in sample_list
+print(f"Single Status  : {bool_result}")
